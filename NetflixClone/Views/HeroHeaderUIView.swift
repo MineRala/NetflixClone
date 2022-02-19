e3826d4b71bdf35c970351f599c160975eb02f2f
Mine Rala <mine-rala@hotmail.com>
2022-02-19 13:24:36 +0300

    Added play and download button

--- NetflixClone/Views/HeroHeaderUIView.swift
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
     
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+         button.setTitle("Download", for: .normal)
+         button.layer.borderColor = UIColor.white.cgColor
+         button.layer.borderWidth = 1
+         button.layer.cornerRadius = 5
+         button.translatesAutoresizingMaskIntoConstraints = false
+         return button
+    }()
+    
+    private let playButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private let heroImageView : UIImageView = {
        let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -21,6 +41,9 @@ class HeroHeaderUIView: UIView {
         super.init(frame: frame)
         addSubview(heroImageView)
         addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        applyConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -41,6 +64,24 @@ class HeroHeaderUIView: UIView {
         ]
         gradientLayer.frame = bounds
         layer.addSublayer(gradientLayer)
+    }
+    
+    private func applyConstraints() {
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 70),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30),
+            playButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraints)
+        
+        let downloadButtonConstraints = [
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -70),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30),
+            downloadButton.widthAnchor.constraint(equalToConstant: 120)
+        ]
+        
+        NSLayoutConstraint.activate(downloadButtonConstraints)
     }
     
 }
