9d4013e4cc89ee7b451d8b42ea6eeb0b81454ff8
Mine Rala <mine-rala@hotmail.com>
2022-02-22 20:26:17 +0300

    Upcoming scene implemented

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Mine Rala on 22.02.2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterUrl: String
+}
