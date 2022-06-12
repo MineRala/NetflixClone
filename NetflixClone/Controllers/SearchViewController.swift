b539d9d92ffb999d54d33b0a2e1ef1e2c36bd430
Mine Rala <mine-rala@hotmail.com>
2022-06-12 12:40:13 +0300

    searchViewController added.

--- NetflixClone/Controllers/SearchViewController.swift
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private let searchTableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchTableView)
+        
+        searchTableView.dataSource = self
+        searchTableView.delegate = self
 
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchTableView.frame = view.bounds
+    }
+}
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Mine"
+        return cell
+    }
+    
+    
 }
